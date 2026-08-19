c4afa584f80bb53de58472da13b75580750994d8
Sanskar <sanskarin@outlook.in>
2026-08-19 08:39:29 +0530

    fix(benchmark): add test runner annotation dependency

--- macrobenchmark/build.gradle.kts
+++ macrobenchmark/build.gradle.kts
@@ -33,4 +33,5 @@ android {
 dependencies {
     implementation(libs.androidx.benchmark.macro.junit4)
     implementation(libs.androidx.test.ext.junit)
+    implementation(libs.androidx.test.runner)
 }
