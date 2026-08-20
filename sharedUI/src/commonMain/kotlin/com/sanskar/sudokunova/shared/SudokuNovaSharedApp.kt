145ce31fbb43cf79cb9ffd39661a759ddb7c3e7d
Sanskar <sanskarin@outlook.in>
2026-08-20 06:43:48 +0530

    fix(shared): import Compose weight layout scope

--- sharedUI/src/commonMain/kotlin/com/sanskar/sudokunova/shared/SudokuNovaSharedApp.kt
+++ sharedUI/src/commonMain/kotlin/com/sanskar/sudokunova/shared/SudokuNovaSharedApp.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -30,7 +31,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
