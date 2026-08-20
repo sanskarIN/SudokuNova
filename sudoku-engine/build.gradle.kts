3fc61afe2f880330d0f1cfaba3f5c4e3314dc8b9
Sanskar <sanskarin@outlook.in>
2026-08-20 06:41:17 +0530

    refactor(engine): make Sudoku engine Kotlin Multiplatform

--- sudoku-engine/build.gradle.kts
+++ sudoku-engine/build.gradle.kts
@@ -1,12 +1,48 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
 }
 
 kotlin {
-    jvmToolchain(17)
-}
+    android {
+        namespace = "com.sanskar.sudokunova.engine"
+        compileSdk = 37
+        minSdk = 26
+        compilerOptions.configure {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
 
-dependencies {
-    testImplementation(kotlin("test-junit"))
-    testImplementation(libs.junit4)
+    jvm("desktop") {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+
+    iosArm64()
+    iosSimulatorArm64()
+
+    js(IR) {
+        browser()
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+    }
+
+    sourceSets {
+        commonMain {
+            kotlin.srcDir("src/main/kotlin")
+        }
+        commonTest {
+            kotlin.srcDir("src/test/kotlin")
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
 }
