18bf67cf1da7c3f9173e8247756dab33830c2b95
Sanskar <sanskarin@outlook.in>
2026-08-24 08:24:16 +0530

    feat(ios): restore and autosave shared game

--- sharedUI/src/iosMain/kotlin/com/sanskar/sudokunova/shared/ios/MainViewController.kt
+++ sharedUI/src/iosMain/kotlin/com/sanskar/sudokunova/shared/ios/MainViewController.kt
@@ -1,9 +1,16 @@
 package com.sanskar.sudokunova.shared.ios
 
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
+import com.sanskar.sudokunova.shared.EncodedSharedGameStore
 import com.sanskar.sudokunova.shared.SudokuNovaSharedApp
+import com.sanskar.sudokunova.shared.rememberPersistedSharedGameState
 import platform.UIKit.UIViewController
 
 fun MainViewController(): UIViewController = ComposeUIViewController {
-    SudokuNovaSharedApp()
+    val gameStore = remember {
+        EncodedSharedGameStore(AppleUserDefaultsGameTextStore())
+    }
+    val state = rememberPersistedSharedGameState(gameStore)
+    SudokuNovaSharedApp(state = state)
 }
