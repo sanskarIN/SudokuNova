533718b6f3785acbf3fbdf40b1773f4fb112d154
Sanskar <sanskarin@outlook.in>
2026-08-24 08:24:07 +0530

    feat(web): restore and autosave shared game

--- sharedUI/src/wasmJsMain/kotlin/com/sanskar/sudokunova/shared/web/Main.kt
+++ sharedUI/src/wasmJsMain/kotlin/com/sanskar/sudokunova/shared/web/Main.kt
@@ -1,12 +1,19 @@
 package com.sanskar.sudokunova.shared.web
 
+import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import com.sanskar.sudokunova.shared.EncodedSharedGameStore
 import com.sanskar.sudokunova.shared.SudokuNovaSharedApp
+import com.sanskar.sudokunova.shared.rememberPersistedSharedGameState
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport(viewportContainerId = "webApp") {
-        SudokuNovaSharedApp()
+        val gameStore = remember {
+            EncodedSharedGameStore(WebLocalStorageGameTextStore())
+        }
+        val state = rememberPersistedSharedGameState(gameStore)
+        SudokuNovaSharedApp(state = state)
     }
 }
