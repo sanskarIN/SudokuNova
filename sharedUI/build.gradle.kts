fe81fdca2e899bd80f844b32f5546b7a820b09aa
Sanskar <sanskarin@outlook.in>
2026-08-24 18:52:00 +0530

    release(desktop): promote package version to 2.0.13

--- sharedUI/build.gradle.kts
+++ sharedUI/build.gradle.kts
@@ -83,7 +83,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "SudokuNova"
-            packageVersion = "2.0.12"
+            packageVersion = "2.0.13"
             description = "SudokuNova cross-platform Sudoku application"
             vendor = "Sanskar"
         }
